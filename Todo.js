fd99506ecc8328edbb0f8b869e874a07acbb7846
HellGround <dlgudwns783@gmail.com>
2022-08-25 14:01:30 +0900

    init

---
+++ Todo.js
@@ -0,0 +1,35 @@
+/**
+ * @class
+ * @constructor
+ * @public
+ */
+class Todo {}
+
+/**
+ *
+ * @param {{
+ *  내용:String,
+ *  완료여부: Boolean,
+ *  카테고리: String,
+ *  [태그들]: String
+ * }} todo
+ */
+function _create(todo) {}
+
+/**
+ *
+ * @param {String} todoId
+ */
+function _read(todoId) {}
+
+/**
+ *
+ * @param {String} todoId
+ */
+function _update(todoId) {}
+
+/**
+ *
+ * @param {String} todoId
+ */
+function _delete(todoId) {}
